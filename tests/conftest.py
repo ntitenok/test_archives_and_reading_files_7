2290dd233c72e89830fd77c6e5dd63e7f9d2f1d8
Nikolai Titenok <titenok.n@yandex.ru>
2024-02-12 20:57:28 +0300

    Добавил создание и удаление папки из каталога

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,15 @@
 import os.path
+import shutil
+
 import pytest
 from zipfile import ZipFile, ZIP_DEFLATED
-from utilites.archive_pathes import TESTDATA_PATH, ARCHIVE_PATCH
+from utilites.archive_pathes import TESTDATA_PATH, ARCHIVE_PATCH, EXTRACTED_FILES_PATCH
 
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=True,scope='session')
 def test_create_archive():
+    os.mkdir(EXTRACTED_FILES_PATCH)
     with ZipFile(ARCHIVE_PATCH, 'a') as my_zip:
         for folder, subfolders, files in os.walk(str(TESTDATA_PATH)):
             for file in files:
@@ -14,3 +17,5 @@ def test_create_archive():
 
     yield
     os.remove(ARCHIVE_PATCH)
+    shutil.rmtree(EXTRACTED_FILES_PATCH)
+
